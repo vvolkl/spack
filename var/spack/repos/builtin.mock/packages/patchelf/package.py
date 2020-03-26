bdd8fac5b39f7c05db9677a2425fc013cbf139d6
Patrick Gartung <gartung@fnal.gov>
2020-03-25 23:41:55 -0500

    Buildcache: Try mocking an install of quux, corge and garply using prebuilt binaries

--- var/spack/repos/builtin.mock/packages/patchelf/package.py
+++ var/spack/repos/builtin.mock/packages/patchelf/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class Patchelf(AutotoolsPackage):
+class Patchelf(Package):
     """
     PatchELF is a small utility to modify the
     dynamic linker and RPATH of ELF executables.
@@ -18,5 +18,10 @@ class Patchelf(AutotoolsPackage):
     list_url = "http://nixos.org/releases/patchelf/"
     list_depth = 1
 
+    version('0.10', '976539c80f28b0ac6807e93b7ec8fd6a1fb68e474f316de464b6f638095e16fd',
+        url='https://github.com/gartung/patchelf/releases/download/0.10/patchelf-0.10.tar.gz')
     version('0.9', '3c265508526760f233620f35d79c79fc')
     version('0.8', '407b229e6a681ffb0e2cdd5915cb2d01')
+
+    def install(self, spec, prefix):
+        install_tree(self.stage.source_path, prefix)
