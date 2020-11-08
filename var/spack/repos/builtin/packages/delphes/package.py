d22a79cfa08aaf9306f698f650bc5f00391ffa04
Valentin Volkl <vavolkl@k4-centos7-01.cern.ch>
2020-11-08 17:26:45 +0100

    add delphes prerelease

--- var/spack/repos/builtin/packages/delphes/package.py
+++ var/spack/repos/builtin/packages/delphes/package.py
@@ -18,6 +18,7 @@ class Delphes(CMakePackage):
     maintainers = ['drbenmorgan', 'vvolkl', 'selvaggi']
 
     version('master', branch='master')
+    version('3.4.3pre06', tag='3.4.3pre06')
     version('3.4.3pre05', tag='3.4.3pre05')
     version('3.4.3pre04', tag='3.4.3pre04')
     version('3.4.2', sha256='d46a7c5474de650befdb89377115feee31f1743107ceb3d8da699be9d48c097b', preferred=True)
