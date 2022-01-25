d2586bd70f60df2283512619a4efd78d9dc0984e
Valentin Volkl <valentin.volkl@cern.ch>
2022-01-25 15:47:01 +0100

    Update package.py

--- var/spack/repos/builtin/packages/edm4hep/package.py
+++ var/spack/repos/builtin/packages/edm4hep/package.py
@@ -33,7 +33,7 @@ class Edm4hep(CMakePackage):
     depends_on('python', type='build')
 
     depends_on('root@6.08:')
-    depends_on('podio@0.14:', when='@0.4:')
+    depends_on('podio@0.15:', when='@0.4:')
     depends_on('podio@0.13.0:0.13', when='@:0.3')
 
     depends_on('hepmc@:2', type='test', when='@:0.4.0')
